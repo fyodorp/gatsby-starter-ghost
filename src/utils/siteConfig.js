4e9d98895c61646e6c863bd12a3b55e995fe3ba8
Fyodor Pimenov <fyodorp@gmail.com>
2020-08-25 23:23:38 -0700

    Update siteConfig.js

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: `http://peaceful-haibt-5647f5.netlify.app:80`, // Site domain. Do not include a trailing slash!
+    siteUrl: `https://condescending-nightingale-d88dfa.netlify.app`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
